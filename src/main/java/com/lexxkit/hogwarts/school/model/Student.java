aa7f6d85962caf56f6686ad99062db21d9778c76
Lexxkit <lexxkit@gmail.com>
2022-08-17 17:14:15 +0300

    added controllers to Student and Faculty models;

--- src/main/java/com/lexxkit/hogwarts/school/model/Student.java
+++ src/main/java/com/lexxkit/hogwarts/school/model/Student.java
@@ -7,6 +7,12 @@ public class Student {
     private String name;
     private int age;
 
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
     public Long getId() {
         return id;
     }
