8422ea1746c26624968d7a7ed7ea091becdc0791
Lexxkit <lexxkit@gmail.com>
2022-08-17 16:59:07 +0300

    Init

---
+++ src/main/java/com/lexxkit/hogwarts/school/SkyproHogwartsApplication.java
@@ -0,0 +1,13 @@
+package com.lexxkit.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHogwartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lexxkit/hogwarts/school/SkyproHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexxkit.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
