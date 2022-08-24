f7fc8c69d9140965b359e4c43d14a033f1498e54
Lexxkit <lexxkit@gmail.com>
2022-08-24 16:33:20 +0300

    created scripts.sql with 5 SQL requests;

---
+++ scripts.sql
@@ -0,0 +1,9 @@
+select * from student where age >= 12 and age <= 13;
+
+select name from student;
+
+select * from student where name like '%n%';
+
+select * from student where age < 13;
+
+select * from student order by age;
