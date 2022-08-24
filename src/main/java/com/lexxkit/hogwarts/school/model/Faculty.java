a2576722b6076b4cb5a06cc84829cba1b8d748df
Lexxkit <lexxkit@gmail.com>
2022-08-24 16:53:09 +0300

    added many-to-one relationship between Student and Faculty;

--- src/main/java/com/lexxkit/hogwarts/school/model/Faculty.java
+++ src/main/java/com/lexxkit/hogwarts/school/model/Faculty.java
@@ -1,10 +1,8 @@
 package com.lexxkit.hogwarts.school.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Faculty {
@@ -14,13 +12,17 @@ public class Faculty {
     private String name;
     private String color;
 
+    @OneToMany(mappedBy = "faculty")
+    private Set<Student> students;
+
     public Faculty() {
     }
 
-    public Faculty(Long id, String name, String color) {
+    public Faculty(Long id, String name, String color, Set<Student> students) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.students = students;
     }
 
     public Long getId() {
@@ -45,6 +47,14 @@ public class Faculty {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
     }
 
     @Override
