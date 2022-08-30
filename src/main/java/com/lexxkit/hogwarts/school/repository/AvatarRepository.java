6543eef8a8091bcb935a3f94a1652b613c0844df
Lexxkit <lexxkit@gmail.com>
2022-08-30 13:09:37 +0300

    added methods to AvatarService;

--- src/main/java/com/lexxkit/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/com/lexxkit/hogwarts/school/repository/AvatarRepository.java
@@ -4,6 +4,9 @@ import com.lexxkit.hogwarts.school.model.Avatar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+    Optional<Avatar> findByStudentId(Long studentId);
 }
--- src/main/java/com/lexxkit/hogwarts/school/service/AvatarService.java
+++ src/main/java/com/lexxkit/hogwarts/school/service/AvatarService.java
@@ -1,7 +1,43 @@
 package com.lexxkit.hogwarts.school.service;
 
+import com.lexxkit.hogwarts.school.model.Avatar;
+import com.lexxkit.hogwarts.school.model.Student;
+import com.lexxkit.hogwarts.school.repository.AvatarRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
+@Transactional
 public class AvatarService {
+
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+    private final AvatarRepository avatarRepository;
+    private final StudentService studentService;
+
+    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
+        this.avatarRepository = avatarRepository;
+        this.studentService = studentService;
+    }
+
+    public Avatar findAvatar(Long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        Student student = studentService.findStudent(studentId);
+        Path filePath = Path.of(avatarsDir, student + "." + getFileExtension(avatarFile.getOriginalFilename()));
+
+        Files.createDirectories(filePath.getParent());
+    }
+
+    private String getFileExtension(String originalFilename) {
+        return originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+    }
 }
