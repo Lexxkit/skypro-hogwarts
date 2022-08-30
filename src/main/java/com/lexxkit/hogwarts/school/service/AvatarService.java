743a6cca7180c434aaca6a9fdb84c31828a10961
Lexxkit <lexxkit@gmail.com>
2022-08-30 16:21:09 +0300

    added 3 endpoints to AvatarController;

--- src/main/java/com/lexxkit/hogwarts/school/service/AvatarService.java
+++ src/main/java/com/lexxkit/hogwarts/school/service/AvatarService.java
@@ -8,13 +8,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 @Transactional
 public class AvatarService {
+
+    private final int PREVIEW_WIDTH = 100;
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
@@ -35,6 +42,43 @@ public class AvatarService {
         Path filePath = Path.of(avatarsDir, student + "." + getFileExtension(avatarFile.getOriginalFilename()));
 
         Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+            bis.transferTo(bos);
+        }
+        Avatar avatar = findAvatar(studentId);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(generatePreview(filePath));
+
+        avatarRepository.save(avatar);
+    }
+
+    private byte[] generatePreview(Path filePath) throws IOException {
+        try (
+                InputStream is = Files.newInputStream(filePath);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream()
+                ) {
+            BufferedImage image = ImageIO.read(bis);
+
+            int height = image.getHeight() / (image.getWidth() / 100);
+            BufferedImage preview = new BufferedImage(PREVIEW_WIDTH, height, image.getType());
+            Graphics2D graphics = preview.createGraphics();
+            graphics.drawImage(image, 0, 0, PREVIEW_WIDTH, height, null);
+            graphics.dispose();
+
+            ImageIO.write(preview, getFileExtension(filePath.getFileName().toString()), baos);
+            return baos.toByteArray();
+        }
     }
 
     private String getFileExtension(String originalFilename) {
