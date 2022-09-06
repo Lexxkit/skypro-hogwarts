d4e70ba45725d1f73746f234d9022b0d9db30f72
Lexxkit <lexxkit@gmail.com>
2022-09-06 11:35:34 +0300

    added some new tests fot FacultyControllerTests;

--- src/test/java/com/lexxkit/hogwarts/school/controller/FacultyControllerTests.java
+++ src/test/java/com/lexxkit/hogwarts/school/controller/FacultyControllerTests.java
@@ -1,5 +1,6 @@
 package com.lexxkit.hogwarts.school.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lexxkit.hogwarts.school.model.Faculty;
 import com.lexxkit.hogwarts.school.repository.FacultyRepository;
 import com.lexxkit.hogwarts.school.service.FacultyService;
@@ -16,15 +17,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
-import static com.lexxkit.hogwarts.school.TestConstants.GRIFFINDOR;
+import static com.lexxkit.hogwarts.school.TestConstants.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(FacultyController.class)
+@WebMvcTest(controllers = FacultyController.class)
 class FacultyControllerTests {
 
     @Autowired
@@ -35,6 +35,9 @@ class FacultyControllerTests {
 
     @SpyBean
     private FacultyService facultyService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @InjectMocks
     private FacultyController facultyController;
@@ -58,6 +61,35 @@ class FacultyControllerTests {
                 .andExpect(jsonPath("$.id").value(GRIFFINDOR.getId()))
                 .andExpect(jsonPath("$.name").value(GRIFFINDOR.getName()))
                 .andExpect(jsonPath("$.color").value(GRIFFINDOR.getColor()));
+    }
+
+    @Test
+    void getFacultiesByColor() throws Exception {
+        when(facultyRepository.findByColor(COLOR)).thenReturn(List.of(SLYTHERIN));
+
+        mockMvc.perform(get("/faculties")
+                        .queryParam("color", COLOR)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(SLYTHERIN))));
+    }
+
+    @Test
+    void getFacultiesByColorOrNameIgnoreCase() throws Exception {
+        String colorIgnoreCase = SLYTHERIN.getColor().toUpperCase();
+        String nameIgnoreCase = GRIFFINDOR.getName().toUpperCase();
+
+        when(facultyRepository.findFacultyByNameIgnoreCaseOrColorIgnoreCase(nameIgnoreCase, colorIgnoreCase))
+                .thenReturn(List.of(SLYTHERIN, GRIFFINDOR));
+
+        mockMvc.perform(get("/faculties")
+                        .queryParam("color", colorIgnoreCase)
+                        .queryParam("name", nameIgnoreCase)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(SLYTHERIN, GRIFFINDOR))));
     }
 
     @Test
@@ -94,10 +126,31 @@ class FacultyControllerTests {
     }
 
     @Test
+    void updateFacultyTest() throws Exception {
+        JSONObject updGriffindorObject = new JSONObject();
+        updGriffindorObject.put("name", UPD_GRIFFINDOR.getName());
+        updGriffindorObject.put("color", UPD_GRIFFINDOR.getColor());
+
+        when(facultyRepository.findById(GRIFFINDOR.getId())).thenReturn(Optional.of(GRIFFINDOR));
+        when(facultyRepository.save(any(Faculty.class))).thenReturn(UPD_GRIFFINDOR);
+
+        mockMvc.perform(put("/faculties")
+                        .content(updGriffindorObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(UPD_GRIFFINDOR.getId()))
+                .andExpect(jsonPath("$.name").value(UPD_GRIFFINDOR.getName()))
+                .andExpect(jsonPath("$.color").value(UPD_GRIFFINDOR.getColor()));
+    }
+
+    @Test
     void deleteFacultyTest() throws Exception{
         doNothing().when(facultyRepository).deleteById(anyLong());
 
         mockMvc.perform(delete("/faculties/" + GRIFFINDOR.getId()))
                 .andExpect(status().isOk());
+
+        verify(facultyRepository, atLeastOnce()).deleteById(anyLong());
     }
 }
