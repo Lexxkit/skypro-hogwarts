f7b81f1ed99a646778896400d69108092135dc95
Lexxkit <lexxkit@gmail.com>
2022-09-12 17:33:28 +0300

    created two changesets with indexes for students name and faculty name_color;

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,2 +1,7 @@
 -- liquibase formatted sql
 
+-- changeset oalekseenko:1
+create index student_name_index on student (name);
+
+-- changeset oalekseenko:2
+create index faculty_nc_index on faculty (name, color);
